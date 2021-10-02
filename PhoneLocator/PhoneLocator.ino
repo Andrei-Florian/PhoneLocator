0aed6ecc573b30c3b1a5e84678df956a95fbef6a
Andrei Florian <andrei_florian@universumco.com>
2021-10-02 20:02:33 +0100

    refactor code

--- PhoneLocator/PhoneLocator.ino
+++ PhoneLocator/PhoneLocator.ino
@@ -1,150 +1,107 @@
-// Universum | Universum Projects > phoneLocator
+#define BLYNK_MAX_SENDBYTES 256
 
-// Andrei Florian 10/FEB/2018
-
-  // 1.1 Andrei Florian 10/SEP/2018
-  
 #include <stdio.h>
 #include <Bridge.h>
 #include <BlynkSimpleYun.h>
 
-char auth[] = "5b15d89f12514c3e8eff04b6287e1335";   // TODO - insert Blynk auth token here
-char userEmail[] = "lightpro64@gmail.com";            // TODO - insert your email here
+char auth[] = "L6M732Z7WKMNZEornpb7oHADwD3WY1hp"; // TODO - insert Blynk auth token here
 
-float latitude;
-float longitude;
-double altitude;
-double speed;
+double latitude;
+double longitude;
 
-int i;
-int proDebug = 1; // TODO - set to 0 if not debugging
-int buttonPin = 2;
-int online;
+const int buttonPin = 2;
+const int ledPin = LED_BUILTIN;
 
-BLYNK_WRITE(V0) // read virtual pin 0 for co-ordinates
+// runs in the background to populate the geolocation fields
+BLYNK_WRITE(V0)
 {
-  latitude = param[0].asDouble();
-  longitude = param[1].asDouble();
-  altitude = param[2].asDouble();
-  speed = param[3].asDouble();
+	latitude = param[0].asDouble();
+	longitude = param[1].asDouble();
 }
 
-
-
-
-void getLocation()
+// check if the button is pressed
+bool getButtonState(int _buttonPin)
 {
-  if(digitalRead(buttonPin) == HIGH)
-  {
-    online = 1;
-    if(proDebug == 1)
-    {
-      Serial.println("button pressed");
-      Serial.println("Locating Phone");
-      Serial.println("");
-    }
-  }
-  
-  if(i < 10) // if requested co-ordinates less than 10 times
-  {
-    if(online == 1)
-    {
-      if(latitude != 0.00 && longitude != 0.00) // if co-ordinates are valid
-      {
-        //String toSend is the string that will be sent as the emails body
-        String toSend = "Phone Located! \n received co-ordinates \n LAT ";
-        toSend += String(latitude, 8); // add vaiables to string
-        toSend += " LNG ";
-        toSend += String(longitude, 8);
-        toSend += ". View location on Maps: ";
-        toSend += "www.google.com/maps/?q=";
-        toSend += String(latitude, 8);
-        toSend += ",";
-        toSend += String(longitude, 8);
-        delay(500);
+	if (digitalRead(_buttonPin) == HIGH)
+	{
+		delay(1000);
+		while (digitalRead(_buttonPin) == HIGH)
+			;
+		return true;
+	}
 
-        if(proDebug == 1)
-        {
-          Serial.println("Phone geolocation found");
-          Serial.println("Sending Email...");
-          Serial.println(toSend);
-        }
-      
-        Blynk.email("lightpro64@gmail.com", "PhoneLocator | Notification", toSend); // send email
-
-        if(proDebug == 1)
-        {
-          Serial.println("Success");
-          Serial.println("Email Sent");
-          Serial.println("Restarting Protocol");
-          Serial.println("");
-        } 
-      
-        delay(5000);
-        i = 0;
-        online = 0;
-      }
-      else
-      {
-        if(i > 9)
-        {
-          if(proDebug == 1)
-          {
-            Serial.println("Error - Timeout Occured in Communication");
-            delay(500);
-            Serial.println("Restarting");
-          }
-    
-          i = 0;
-          online = 0;
-          delay(1000);
-        }
-        else if(i < 9)
-        {
-          i++;
-          delay(1000);
-          if(proDebug == 1)
-          {
-            Serial.println("Geolocation not Received");
-            Serial.println("Blynk responded with");
-            Serial.println(latitude, 8);
-            Serial.println(longitude, 8);
-            Serial.println("");
-          }
-        }
-      }
-    }
-  }
+	return false;
 }
 
+// check if the location fields are populated
+bool checkLocation(double _latitude, double _longitude)
+{
+	if (_latitude == 0 || _longitude == 0)
+	{
+		return false;
+	}
 
+	return true;
+}
 
+// format the message to be sent
+String formatMessage(double _latitude, double _longitude)
+{
+	String message = "Phone Located! \n received co-ordinates \n LAT ";
+	message += String(_latitude, 8); // add vaiables to string
+	message += " LNG ";
+	message += String(_longitude, 8);
+	message += ". View location on Maps: ";
+	message += "www.google.com/maps/?q=";
+	message += String(_latitude, 8);
+	message += ",";
+	message += String(_longitude, 8);
 
+	return message;
+}
+
+// interact with the API to send the email
+void sendMessage(String _message)
+{
+	Serial.println("[Locate] Sent location");
+	Serial.println(_message);
+	Serial.println("");
+
+	Blynk.email("PhoneLocator", _message); // send email
+}
 
 void setup()
 {
-  pinMode(buttonPin, INPUT);
-  Blynk.begin(auth); // initialise Blynk
-  delay(1000);
-  Blynk.email("lightpro64@gmail.com", "PhoneLocator | Notification", "Project Online"); // send email
+	pinMode(buttonPin, INPUT); // set the button pin as an input
+	pinMode(ledPin, OUTPUT);   // set the led as an output
+	Blynk.begin(auth);		   // initialise Blynk
 
-  if(proDebug == 1)
-  {
-    Serial.begin(9600);
-    while(!Serial) {};
-    delay(1000);
-    Serial.println("phoneLocator Online");
-    Serial.println("");
-    Serial.println("For this project to work, ensure that");
-    Serial.println("  Phone is connected to WiFi or Mobile Data");
-    Serial.println("");
-    Serial.println("");
-    delay(500);
-  }
+	Serial.begin(9600);
+	Serial.println("[INFO] phoneLocator Online");
+
+	Serial.println("[INFO] Connecting to Phone");
+	while (!checkLocation(latitude, longitude)) // wait until the device streams its location
+	{
+		Blynk.run();
+
+		digitalWrite(ledPin, HIGH);
+		delay(1000);
+		digitalWrite(ledPin, LOW);
+		delay(1000);
+	}
+
+	Serial.println("[INFO] Connected to Phone");
+	digitalWrite(ledPin, HIGH);
 }
 
 void loop()
 {
-  Blynk.run();
-  getLocation();
+	Blynk.run();
+
+	if (getButtonState(buttonPin) && checkLocation(latitude, longitude))
+	{
+		sendMessage(formatMessage(latitude, longitude));
+	}
+
+	delay(10);
 }
